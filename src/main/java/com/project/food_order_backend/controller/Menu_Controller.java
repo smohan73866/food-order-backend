96b4a85fc312f967fb6d469b788888566d977d55
Tharun <tharunkummara2003@gmail.com>
2026-05-05 16:06:10 +0530

    updated

---
+++ src/main/java/com/project/food_order_backend/controller/Menu_Controller.java
@@ -0,0 +1,27 @@
+package com.project.food_order_backend.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.project.food_order_backend.model.MenuItem;
+import com.project.food_order_backend.service.Menu_Service;
+
+@RestController
+@RequestMapping("/menu")
+public class Menu_Controller {
+
+    @Autowired
+    private Menu_Service menuService;
+
+    @GetMapping
+    public List<MenuItem> getMenu() {
+        return menuService.getAllMenuItems();
+    }
+
+    @PostMapping
+    public MenuItem addMenuItem(@RequestBody MenuItem item) {
+        return menuService.addMenuItem(item);
+    }
+}
---
+++ src/main/java/com/project/food_order_backend/controller/Restaurant_Controller.java
@@ -0,0 +1,27 @@
+package com.project.food_order_backend.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.project.food_order_backend.model.Restaurant;
+import com.project.food_order_backend.service.Restaurant_Service;
+
+@RestController
+@RequestMapping("/restaurants")
+public class Restaurant_Controller {
+
+    @Autowired
+    private Restaurant_Service restaurantService;
+
+    @GetMapping
+    public List<Restaurant> getRestaurants() {
+        return restaurantService.getAllRestaurants();
+    }
+
+    @PostMapping
+    public Restaurant addRestaurant(@RequestBody Restaurant restaurant) {
+        return restaurantService.addRestaurant(restaurant);
+    }
+}
