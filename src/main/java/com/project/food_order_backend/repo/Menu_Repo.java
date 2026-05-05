aef484287ac7caf7507e6fa94d81441b50986c82
Tharun <tharunkummara2003@gmail.com>
2026-05-05 12:40:02 +0530

    updated

---
+++ src/main/java/com/project/food_order_backend/repo/Menu_Repo.java
@@ -0,0 +1,21 @@
+package com.project.food_order_backend.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+import com.project.food_order_backend.model.MenuItem;
+
+@Repository
+public interface Menu_Repo extends JpaRepository<MenuItem, Long> {
+
+    // Find menu items by name
+    List<MenuItem> findByName(String name);
+
+    // Find menu items cheaper than given price
+    List<MenuItem> findByPriceLessThan(double price);
+
+    // Find menu items by category
+    List<MenuItem> findByCategory(String category);
+
+}
